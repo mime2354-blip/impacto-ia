97c32016961282aee232f311d5551f54cf7ea84c
mime2354-blip <mime2354@gmail.com>
2026-09-05 10:45:34 +0200

    Update generar.js

--- api/generar.js
+++ api/generar.js
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
   res.status(200).send("FUNCION API OK");
 }
-    return res.status(500).json({
-      error: error.message || "Error interno del servidor"
-    });
-  }
-}
